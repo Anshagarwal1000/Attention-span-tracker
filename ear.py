b7b527b2dfb0959e68bd6de57db0f066052be48e
Agam verma <agamverma00@gmail.com>
2025-06-23 14:32:43 +0530

    i change

--- ear.py
+++ ear.py
@@ -3,7 +3,7 @@ from scipy.spatial import distance as dist
 def calculate_ear(eye):
     """Calculate the Eye Aspect Ratio (EAR) for a given eye."""
     A = dist.euclidean(eye[1], eye[5])
-    B = dist.euclidean(eye[2], eye[4])
+    B = dist.euclidean(eye[2], eye[4])vjhv
     C = dist.euclidean(eye[0], eye[3])
     ear = (A + B) / (2.0 * C)
     return ear
